a2321a8ce7a35337c234be55bc09b5875dfe08ab
Tom Kiefhaber <tkiefhab@gmail.com>
2014-08-07 12:29:10 -0400

    add averages

--- app/models/report.rb
+++ app/models/report.rb
@@ -12,4 +12,56 @@ class Report < ActiveRecord::Base
   def percent_red
     lines.red.count.to_f / lines.count.to_f
   end
+
+  def green_average
+    @green_average ||= ViewObj.new(self, :green)
+  end
+
+  def yellow_average
+    @yellow_average ||= ViewObj.new(self, :yellow)
+  end
+
+  def red_average
+    @red_average ||= ViewObj.new(self, :red)
+  end
+
+
+  ViewObj = Struct.new(:report, :scope) do
+
+    def lines
+      @lines ||= report.lines.send(scope)
+    end
+
+    def avg_original_list_price
+      lines.average(:original_list_price)
+    end
+
+    def avg_list_price
+      lines.average(:list_price)
+    end
+
+    def avg_sale_price
+      lines.average(:sale_price)
+    end
+
+    def avg_olp_to_sp_percentage
+      lines.average(:olp_to_sp_percentage)
+    end
+
+    def avg_days_on_market
+      lines.average(:days_on_market).to_i
+    end
+
+    def avg_rooms
+      lines.average(:rooms).to_i
+    end
+
+    def avg_beds
+      lines.average(:beds).to_i
+    end
+
+    def avg_baths
+      lines.average(:baths).to_i
+    end
+  end
 end
