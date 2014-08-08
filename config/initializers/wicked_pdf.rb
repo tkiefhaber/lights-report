4b8d180b5c689dec0d2409fd9672035316dd0271
Tom Kiefhaber <tkiefhab@gmail.com>
2014-08-07 23:43:19 -0400

    another try

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,5 @@
 if Rails.env.staging? || Rails.env.production?
-  WickedPdf.config = {:exe_path => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s}
+  WickedPdf.config = {:exe_path => config.exe_path = Rails.root.to_s + "/bin/wkhtmltopdf"}
 else
   WickedPdf.config = { :exe_path => '/usr/local/bin/wkhtmltopdf'}
 end
